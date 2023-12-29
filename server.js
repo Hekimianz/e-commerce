e2666eeb4422e76d0842ff4899565d545ed997b8
Hekimianz <aramhekdesign@gmail.com>
2023-12-29 14:38:49 -0600

    Set up server and database connection

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const { query } = require("./db/index");
+
+app.get("/products", async (req, res) => {
+  try {
+    const result = await query("SELECT * FROM products");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
